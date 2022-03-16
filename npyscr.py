5abefbe52b45d8860021d0d0b409fd940cdd569e
Arkady Shenberg <shenbergarkadii@gmail.com>
2022-03-16 22:55:05 +0700

    Теперь точно готовый скелет
    Размеченный макет с соблюдений пропорций экрана; полноэкранный режим; всплывающее меню и кнопка выхода.

--- npyscr.py
+++ npyscr.py
@@ -1,93 +1,125 @@
-#
-# import npyscreen
-#
-#
-# class App(npyscreen.StandardApp):
-#     def onStart(self):
-#         self.addForm("MAIN", MainForm, name="ХРАМ")
-#
-#
-# class MainForm(npyscreen.ActionForm):
-#     npyscreen.ActionForm.OK_BUTTON_TEXT = 'ВЫХОД'
-#     npyscreen.ActionForm.CANCEL_BUTTON_TEXT = 'бесполезая кнопка'
-#
-#     def create(self):
-#         self.title = self.add(npyscreen.TitleText, name="оно работает", value="спасибо")
-#         self.add(npyscreen.TitleSelectOne, rely=40, max_height=3, value = [1,], name="Pick One",
-#                 values = ["Продолжить","Новая игра","Выйти"], scroll_exit=True)
-#
-#
-#     def on_ok(self):
-#         self.parentApp.setNextForm(None)
-#
-#     def on_cancel(self):
-#         self.title.value = "=)"
-#
-#
-# if __name__ == '__main__':
-#     MyApp = App()
-#     MyApp.run()
-
+# -*- coding: utf-8 -*-
 
 import npyscreen
 import curses
+import os
+from ctypes import *
 
 
 class App(npyscreen.StandardApp):
     def onStart(self):
-        self.addForm("MAIN", MainForm, name="ХРАМ")
+        self.addForm("MAIN", MainForm, name=" Х Р А М ")
 
 
-class InputBox1(npyscreen.BoxTitle):
+class DefaultTheme(npyscreen.ThemeManager):
+    default_colors = {
+        'DEFAULT'     : 'WHITE_BLACK',
+        'FORMDEFAULT' : 'WHITE_BLACK',
+        'NO_EDIT'     : 'WHITE_BLACK',
+        'STANDOUT'    : 'WHITE_BLACK',
+        'CURSOR'      : 'WHITE_BLACK',
+        'CURSOR_INVERSE': 'BLACK_WHITE',
+        'LABEL'       : 'WHITE_BLACK',
+        'LABELBOLD'   : 'WHITE_BLACK',
+        'CONTROL'     : 'WHITE_BLACK',
+        'WARNING'     : 'WHITE_BLACK',
+        'CRITICAL'    : 'WHITE_BLACK',
+        'GOOD'        : 'WHITE_BLACK',
+        'GOODHL'      : 'WHITE_BLACK',
+        'VERYGOOD'    : 'WHITE_BLACK',
+        'CAUTION'     : 'WHITE_BLACK',
+        'CAUTIONHL'   : 'WHITE_BLACK',
+    }
+
+
+class Speaker(npyscreen.BoxTitle):
     _contained_widget = npyscreen.MultiLineEdit
-    # def when_value_edited(self):
-    #     self.parent.parentApp.queue_event(npyscreen.Event("event_value_edited"))
 
 
-class InputBox2(npyscreen.BoxTitle):
-    _contained_widget = npyscreen.MultiLineEdit
+class Inventory(npyscreen.BoxTitle):
+    _contained_widget = npyscreen.MultiLine
 
 
-class MainForm(npyscreen.ActionForm):
-    npyscreen.ActionForm.OK_BUTTON_TEXT = 'ВЫЙТИ'
-    npyscreen.ActionForm.CANCEL_BUTTON_TEXT = 'ы'
+class MainForm(npyscreen.FormBaseNewWithMenus):
 
+    def __init__(self, name=None, parentApp=None, framed=None, help=None,
+                 color='FORMDEFAULT',
+                 widget_list=None, cycle_widgets=False, *args, **keywords):
+        super().__init__(name, parentApp, framed, help, color, widget_list,
+                         cycle_widgets, args, keywords)
+        self.main_menu = None
+        self.speaker = None
+        self.actions = None
+        self.inventory = None
 
+    def draw_form(self):
+        super(npyscreen.FormBaseNewWithMenus, self).draw_form()
+        menu_advert = " " + self.__class__.MENU_KEY + ": М Е Н Ю "
+        if isinstance(menu_advert, bytes):
+            menu_advert = menu_advert.decode('utf-8', 'replace')
+        y, x = self.display_menu_advert_at()
+        self.add_line(y, x,
+                      menu_advert,
+                      self.make_attributes_list(menu_advert, curses.A_NORMAL),
+                      self.columns - x - 1
+                      )
 
     def create(self):
-        self.add_event_hander("event_value_edited", self.event_value_edited)
-        new_handlers = {
-            # Устанавливаем ctrl+Q для выхода
-            "^Q": self.exit_func,
-            # Устанавливаем alt+enter для очистки inputbox
-            # curses.ascii.alt(curses.ascii.NL): self.inputbox_clear
-        }
-        self.add_handlers(new_handlers)
+        npyscreen.setTheme(DefaultTheme)
 
         y, x = self.useable_space()
-        # self.InputBox1 = self.add(InputBox1, max_height=y // 2, )
-        self.InputBox2 = self.add(InputBox2, editable=False, max_height=y // 2)
-        self.add(npyscreen.TitleSelectOne, rely=27, max_height=3, max_value=[1, ],
-                 name="Ваш выбор", values = ["Продолжить","Новая игра","Выйти"], scroll_exit=True)
 
-    def event_value_edited(self, event):
-        self.InputBox2.value = self.InputBox1.value
-        self.InputBox2.display()
+        slots_action = ["1. ...", "2. ...", "3. ..."]
+        slots_inv = ['1. ФОНАРЬ', '2. ...']
+        storytelling = self.text_for_storytel()
 
-    def on_ok(self):
-        self.parentApp.setNextForm(None)
+        self.main_menu = self.new_menu(name=' М Е Н Ю ')
+        self.main_menu.addItem(text=' СОХРАНИТЬ И ВЫЙТИ', onSelect=self.main_menu_save_exit)
+        self.main_menu.addItem(text=' ВЫЙТИ', onSelect=self.main_menu_exit)
 
-    def on_cancel(self):
-        self.title.value = "=)"
+        self.speaker = self.add(Speaker, editable=False, max_height=y//2+1, rely=1, value=storytelling)
 
-    def inputbox_clear(self, _input):
-        self.InputBox1.value = self.InputBox2.value = ""
-        self.InputBox1.display()
-        self.InputBox2.display()
+        self.actions = self.add(npyscreen.SelectOne, editable=True, max_width=x//2-5, rely=y//2+2, values=slots_action)
 
-    def exit_func(self, _input):
+        self.inventory = self.add(Inventory, editable=True, name=' И Н В Е Н Т А Р Ь ', rely=y//2+2, relx=x//2, values=slots_inv)
+
+        # slots = ['+']  # Обновление checkbox'а
+        # self.Inventory.display()
+        # self.Inventory = self.add(npyscreen.TitleSelectOne, editable=True,
+        #                           max_width=x // 2 - 5, rely=27, values=slots,
+        #                           name=" М Е Н Ю ")
+
+    # def event_value_edited(self, event):  # Очистка спикера
+    #     self.InputBox2.value = self.InputBox1.value
+    #     self.InputBox2.display()
+
+    @staticmethod
+    def text_for_storytel():
+        paragraph = 'Двадцатого августа 1917 года я, Карл-Хайнрих, граф фон Альтберг-Эренштейн, командор-лейтенант имперского военного флота, передаю эту бутылку и записи Атлантическому океану в месте, неизвестном мне: вероятно, это 20 градусов северной широты и 35 градусов восточной долготы, где мой корабль беспомощно лежит на океанском дне. Поступаю так в силу моего желания предать гласности некоторые необычные факты: нет вероятности, что я выживу и смогу рассказать об этом сам, потому что окружающие обстоятельства настолько же необычайны, насколько угрожающи, и включают в себя не только безнадежное повреждение У-29, но и совершенно разрушительное ослабление моей немецкой железной воли.'
+        k = 0
+        for i in range(len(paragraph)):
+            if paragraph[i] == ' ':
+                k += 1
+            if k == 13:
+                paragraph = paragraph[:i] + ' \n' + paragraph[i + 1:]
+                k = 0
+        return paragraph
+
+    @staticmethod
+    def main_menu_exit():
         exit(0)
 
+    @staticmethod
+    def main_menu_save_exit():
+        pass
 
-MyApp = App()
-MyApp.run()
+
+if __name__ == '__main__':
+    windll.kernel32.SetConsoleTitleW("XPAM")
+
+    x = (windll.user32.GetSystemMetrics(0)) // 5
+    y = (windll.user32.GetSystemMetrics(1)) // 5
+    os.system('mode con cols=' + str(x) + ' lines=' + str(y))
+
+    MyApp = App()
+    MyApp.run()
