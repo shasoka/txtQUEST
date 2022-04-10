eb1bc6035fb57eb837a6247ad7f5f54a6cbe6b21
infern <semenmochalov55@gmail.com>
2022-04-10 20:32:30 +0700

    Система предметов и поиск фигурки со статуей

--- game.py
+++ game.py
@@ -8,13 +8,15 @@ with open("Map_generator/map.json", "r", encoding="UTF-8") as f:
 
 def description_output(coordinates, mind):
     output = ""
-    raw_output = map_of_world[str(coordinates[0])][str(coordinates[1])]
+    raw_output = map_of_world[coordinates[0]][coordinates[1]]["map"]
     signs = "%&@#*№"
     for char in raw_output:
         if char != " " and random.randint(0, 75) * random.random() > mind:
             output += random.choice(signs)
         else:
             output += char
+    if map_of_world[coordinates[0]][coordinates[1]]["statue"] == 1:
+        output += "\n Огромная статуя, повторяющая изображение на фигурке"
     return output
 
 
@@ -29,11 +31,60 @@ def mind_output(mind):
         return "Нужно продолжать исследования."
 
 
+def show_items(items_dict):
+    for thing, num in items_dict.items():
+        if thing != "map" and thing != "statue" and num[0] != 0:
+            print(thing, ": ", num, sep="")
+
+
+def find_items(items_dict):
+    items = 0
+    for item, amount in items_dict.items():
+        if amount != [0] and item not in ["map", "statue"]:
+            items += 1
+    if items > 0:
+        print("Осмотреть предметы - items")
+
+
+def take_item(name):
+    if name in hero.items and name in map_of_world[hero.coordinates[0]][hero.coordinates[1]]:
+        hero.items[name] += 1
+    elif name in map_of_world[hero.coordinates[0]][hero.coordinates[1]]:
+        hero.items[name] = 1
+    map_of_world[hero.coordinates[0]][hero.coordinates[1]][name][0] -= 1
+
+
+def show_inventory(inventory):
+    for item, num in inventory.items():
+        if num != 0:
+            print(item, ": ", num, sep="")
+
+
 hero = MainHero()
 
+commands = {
+    "move": hero.move,
+    "items": show_items,
+    "take": take_item,
+    "show": show_inventory
+}
+
 while (n := str(input())) != '0':
-    hero.move(n)
-    hero.mind -= 10
+    print(map_of_world[hero.coordinates[0]][hero.coordinates[1]]["statue"], hero.items["figure"])
+    if map_of_world[hero.coordinates[0]][hero.coordinates[1]]["statue"] == 1 and hero.items["figure"] == 1:
+        print("End")
+        break
+    if n in ["up", "down", "right", "left"]:
+        commands["move"](n)
+    elif n == "items":
+        commands[n](map_of_world[hero.coordinates[0]][hero.coordinates[1]])
+        take_item(n := str(input()))
+        continue
+    elif n == "show":
+        commands[n](hero.items)
+        continue
+
     print(hero.coordinates)
     print(description_output(hero.coordinates, hero.mind), "\n")
     print(mind_output(hero.mind))
+    find_items(map_of_world[hero.coordinates[0]][hero.coordinates[1]])
--- main_hero_class.py
+++ main_hero_class.py
@@ -1,11 +1,14 @@
+import random
+
+
 class MainHero:
     coordinates = [5, 0]
 
     def __init__(self):
         self.mind = 100
+        self.items = {"light": 1, "figure": 0}
 
     def move(self, command):
-
         if command == "up" and self.coordinates[1] <= 8:
             self.coordinates[1] += 1
         elif command == "down" and self.coordinates[1] >= 0:
@@ -14,4 +17,7 @@ class MainHero:
             self.coordinates[0] -= 1
         elif command == "right" and self.coordinates[0] <= 8:
             self.coordinates[0] += 1
-
+        if self.items["light"] != 0:
+            self.items["light"] -= 1
+        elif random.randint(0, 100) > 80:
+            self.mind -= 10
